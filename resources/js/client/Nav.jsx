d38b4ac74e698cc6c9660c2be074dc5fd7845b27
Simon Eddy <simon@simoneddy.com.au>
2023-08-09 13:01:13 +1000

    added blog link

--- resources/js/client/Nav.jsx
+++ resources/js/client/Nav.jsx
@@ -8,6 +8,9 @@ function Nav() {
       {routes.map(({ key, path, label }) => (
         <NavLink to={path} key={key}>{label}</NavLink>
       ))}
+      <a href="https://www.basscoastpost.com/christine-grayden" className="hover:underline text-black active:text-cornflower-blue font-heading font-bold text-xl rounded-lg block p-2">
+        Blog
+      </a>
     </NavBar>
   );
 }
