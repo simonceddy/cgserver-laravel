453ff9cd5f1f55e2d75fb3fcaa5d7706e64cae5e
Simon Eddy <simon@simoneddy.com.au>
2023-08-09 12:40:56 +1000

    removed unrequired routes

--- resources/js/admin/routes.jsx
+++ resources/js/admin/routes.jsx
@@ -1,30 +1,23 @@
 import { createRef } from 'react';
 import ListPages from './features/pages/ListPages';
 import EditPage from './features/pages/EditPage';
-import CreatePage from './features/pages/CreatePage';
+// import CreatePage from './features/pages/CreatePage';
 
 const routes = [
   {
-    key: 'home',
-    nodeRef: createRef(),
-    path: '/',
-    element: <div>Home</div>,
-    label: 'Dashboard',
-  },
-  {
     key: 'pages',
     nodeRef: createRef(),
-    path: '/pages',
+    path: '/',
     element: <ListPages />,
     label: 'Pages',
   },
-  {
-    key: 'createPage',
-    nodeRef: createRef(),
-    path: '/create/page',
-    element: <CreatePage />,
-    label: 'Create Page',
-  },
+  // {
+  //   key: 'createPage',
+  //   nodeRef: createRef(),
+  //   path: '/create/page',
+  //   element: <CreatePage />,
+  //   label: 'Create Page',
+  // },
   {
     key: 'editPage',
     nodeRef: createRef(),
