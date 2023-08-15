7b363f7f4f9535308d5d3dc11d376a62df2a0142
Simon Eddy <simon@simoneddy.com.au>
2023-08-15 16:23:53 +1000

    package updates, increased paragraph margins

--- resources/js/client/components/Content/index.jsx
+++ resources/js/client/components/Content/index.jsx
@@ -1,6 +1,8 @@
+import './Content.css';
+
 function Content({ children }) {
   return (
-    <div className="w-full sm:w-11/12 md:w-5/6 lg:w-3/4 xl:w-2/3 min-h-3/4 flex-1 p-2">
+    <div className="w-full sm:w-11/12 md:w-5/6 lg:w-3/4 xl:w-2/3 min-h-3/4 page-content flex-1 p-2">
       {children}
     </div>
   );
