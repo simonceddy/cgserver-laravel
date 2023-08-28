74ff420c7eacae2084ccd6a8896b25daceb60521
Simon Eddy <simon@simoneddy.com.au>
2023-08-29 01:42:03 +1000

    updated content background

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -2,7 +2,7 @@
 
 @section('app')
     <div id="layout-container" class="w-full h-full bg-white dark:bg-black dark:text-aqua-spring text-black whitespace-nowrap overflow-y-auto">
-        <div id="content-resize" class="sm:w-11/12 md:w-5/6 lg:w-3/4 xl:w-2/3 mx-auto w-full min-h-full h-fit col justify-start items-center bg-cornflower-blue/20">
+        <div id="content-resize" class="sm:w-11/12 md:w-5/6 lg:w-3/4 xl:w-2/3 mx-auto w-full min-h-full h-fit col justify-start items-center bg-cornflower-blue/10">
             <header id="header-container" class="w-full h-[25%] min-h-[25%] col justify-center items-center mb-2 border-2 rounded-lg border-dandelion">
                 <h1 class="font-bold md:text-5xl sm:text-4xl text-3xl mt-3 mb-4 px-2 text-center font-heading text-cornflower-blue">
                     Christine Grayden
